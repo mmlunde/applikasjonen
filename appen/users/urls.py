33bd3c38303cfd894da73e557795a1848507ca4c
mmlunde <milly_lunde@hotmail.com>
2015-05-26 11:34:19 +0200

    Added login and logout functions to /users/views.py and linked to /users/urls.py (and also registrer function for later use). Modified 'style.css' and 'login.html' to make it look better.

--- appen/users/urls.py
+++ appen/users/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-
 from users import views
 
 urlpatterns = patterns('',
     url(r'^login$', views.user_login, name='user_login'),
+    url(r'^logout$', views.user_logout, name='user_logout'),
+    url(r'^register$', views.user_register, name='user_register'),
 )
--- appen/users/views.py
+++ appen/users/views.py
@@ -1,6 +1,24 @@
-from django.contrib.auth import authenticate, login 
+from django.contrib.auth import authenticate, login, logout 
 from django.shortcuts import render
-
+from django.shortcuts import redirect
 
 def user_login(request):
-    return render(request, 'users/login.html')
+    context = {}
+    if request.method == 'POST':
+    	username = request.POST['username']
+    	password = request.POST['password']
+    	user = authenticate(username=username, password=password)
+    	if user is not None:
+    		login(request, user)
+    		return redirect('frontpage')
+    	else:
+    		context['login_failed'] = True
+
+    return render(request, 'users/login.html', context)
+
+def user_logout(request):
+	logout(request)
+	return redirect('frontpage')
+
+def user_register(request):
+	pass
