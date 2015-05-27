775e6264b8bad882d8602f1dd7930cca604e37ac
mmlunde <milly_lunde@hotmail.com>
2015-05-27 14:28:49 +0200

    Edited /users/views.py and /users/templates/register.html so that people can register and the information will be saved. Made theme/frontpage.html an extension of 'theme/base.html'. Added urls to the link in the navbar in theme/base.html.

--- appen/users/views.py
+++ appen/users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.shortcuts import redirect
 
@@ -21,4 +22,15 @@ def user_logout(request):
 	return redirect('frontpage')
 
 def user_register(request):
-	pass
+	context = {}
+	if request.method == "POST":
+		user = User()
+		user.fName = request.POST.get('firstname')
+		user.lName = request.POST.get('lastname')
+		user.email = request.POST.get('email')
+		user.username = request.POST.get('username')	
+		user.set_password(request.POST.get('password'))
+		user.save()
+		context['user_saved'] = True
+	return render(request, 'users/register.html', context)
+
