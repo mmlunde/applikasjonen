d589a521fb6ba921e4cf06ba05878fa428c022fe
mmlunde <milly_lunde@hotmail.com>
2015-05-27 15:56:13 +0200

    Removed the content in /users/models.py and added the content in /site_details/models.py. Added a function '__unicode__' in /site_details/models.py.

--- appen/site_details/models.py
+++ appen/site_details/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class User(models.Model):
+	username = models.CharField(max_length=50)
+	email = models.EmailField()
+	firstname = models.CharField(max_length=50)
+	lastname = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return u'%s (%s)' % (self.firstname, self.lastname)
